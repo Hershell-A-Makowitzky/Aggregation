e31116576ba420b479647a5bd4325bca3331c63c
Hershell A. Makowitzky <hershell.makowitzky@pn.me>
2021-09-05 13:06:51 +0200

    Join multiple version of euclid

--- C/Sedgewick/chap_1/euclid.c
+++ C/Sedgewick/chap_1/euclid.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "max_int_size.h"
 
 int gcd(int u, int v) {
 	int t;
@@ -14,23 +13,45 @@ int gcd(int u, int v) {
 	return u == 0 ? v : 1;
 }
 
+void sort(int u, int v, int w) {
+	int tmp;
+	if (u < v) { tmp = u; u = v; v = tmp; } 
+	if (u < w) { tmp = u; u = w; w = v; v = tmp; }
+	if (v < w) { tmp = v; v = w; w = tmp; }
+}
+
+
+int gcd_three(int u, int v, int w) {
+	int result;
+	sort(u, v, w);
+	result = gcd(u, v);
+	if (result == 1) return 1;
+	return gcd(result, w);
+}
+
 int main(void) {
-	int u, v;
+	int u, v, w;
 	first_line:
-	printf("Insert value for numerator: ");
+	printf("Insert value for first number: ");
 	if (scanf("%d", &u) <= 0) {
 		fpurge(stdin);
 		printf("Invalid input\n");
 		goto first_line;
 	}
 	second_line:
-	printf("Insert value for denominator: ");
+	printf("Insert value for second number: ");
 	if (scanf("%d", &v) <= 0) {
 		fpurge(stdin);
 		printf("Invalid input\n");
 		goto second_line;
 	}
-	printf("Greatest common devider: %i\n", gcd(u, v));
-	/*printf("Greatest uint32 value: %u\n", _uint32);*/
+	third_line:
+	printf("Insert value for third number: ");
+	if (scanf("%d", &w) <= 0) {
+		fpurge(stdin);
+		printf("Invalid input\n");
+		goto third_line;
+	}
+	printf("Greatest common divider: %i\n", gcd_three(u, v, w));
 	return 0;
 }
